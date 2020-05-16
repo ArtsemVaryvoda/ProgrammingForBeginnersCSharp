eeee97d66f73beb7054ee3c05f7f6b34160442cc
Artsem_Varyvoda <artemvarivoda@gmail.com>
2020-05-16 20:14:39 +0300

    Initial load

---
+++ First_book/Chapter_1/Listing_01_01/Listing_01_01/HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listing_01_01
+{
+    //Main class of the program
+    class HelloWorld
+    {
+        //Main method of the program
+        static void Main(string[] args)
+        {
+            //Displaying message in the Console window
+            Console.WriteLine("Learning C# language");
+        }
+    }
+}
