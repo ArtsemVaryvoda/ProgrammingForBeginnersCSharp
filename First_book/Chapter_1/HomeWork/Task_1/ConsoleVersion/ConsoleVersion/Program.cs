1b14a514b2f309ea5a7514859f815ea01511e405
Artsem_Varyvoda <artemvarivoda@gmail.com>
2020-05-17 10:22:39 +0300

    Chapter1, 1st task of Homework add

---
+++ First_book/Chapter_1/HomeWork/Task_1/ConsoleVersion/ConsoleVersion/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleVersion
+{
+    class NameSurnameConsoleDemo
+    {
+        static void Main(string[] args)
+        {
+            string name, surname;
+            Console.WriteLine("Please, enter your name");
+            name = Console.ReadLine();
+            Console.WriteLine("Please, enter your surname");
+            surname = Console.ReadLine();
+            Console.WriteLine($"Nice to meet you, {surname} {name}!");
+        }
+    }
+}
---
+++ First_book/Chapter_1/HomeWork/Task_1/DialogWindowsVersion/DialogWindowsVersion/NameSurnameDemo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Windows.Forms;
+
+namespace DialogWindowsVersion
+{
+    class NameSurnameDemo
+    {
+        static void Main()
+        {
+            string name, surname;
+            name = Interaction.InputBox("Please, enter your name.",
+                "Window for name input");
+            surname = Interaction.InputBox("Please, enter your surname.",
+                "Window for surname input");
+            MessageBox.Show($"Hello, {surname} {name}!",
+                "Welcome window",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+    }
+}
